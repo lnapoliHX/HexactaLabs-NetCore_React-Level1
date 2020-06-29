b0de0425596b5f3de3a762783aef5076a47d4249
Elisabet Arratia <earratia@hexacta.com>
2020-06-28 22:19:30 -0300

    Added Frontend for Product type

---
+++ Stock.Web/client-app/src/modules/producttypes/list/container/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+import PropTypes from "prop-types";
+import { getProductTypes, getAll } from "../index";
+import Presentation from "../presentation";
+
+class ProductTypesPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filters: {
+        initials: "",
+        description: "",
+        condition: "AND"
+      }
+    };
+  }
+
+  filterChanged = event => {
+    const newFilters = {
+      ...this.state.filters,
+      [event.target.name]: event.target.value
+    };
+    this.setState({ filters: newFilters });
+  };
+
+  render() {
+    return (
+      <Presentation
+        data={this.props.producttypes}
+        dataLoading={this.props.loading}
+        defaultPageSize={5}
+        filters={this.state.filters}
+        handleFilter={this.filterChanged}
+        clearFilter={this.props.getAll}
+        {...this.props}
+      />
+    );
+  }
+}
+
+ProductTypesPage.propTypes = {
+  producttypes: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => {
+  return { producttypes: getProductTypes(state) };
+};
+
+const mapDispatchToProps = {
+  getAll,
+  push
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductTypesPage);
