07c5cff113a131757e67ba05475e4f54b3f2cd8e
Andres Ramirez <andresramirez82@gmail.com>
2020-09-17 09:31:37 -0300

    nivel 1

---
+++ Stock.Web/client-app/src/modules/products/update/index.js
@@ -0,0 +1,31 @@
+import api from "../../../common/api";
+import { apiErrorToast } from "../../../common/api/apiErrorToast";
+import { setLoading, ActionTypes } from "../list";
+import { toast } from "react-toastify";
+import { goBack } from "connected-react-router";
+
+/* Actions */
+function success(product) {
+  return {
+    type: ActionTypes.UPDATE,
+    product
+  };
+}
+
+export function update(product) {
+  return function(dispatch) {
+    dispatch(setLoading(true));
+    return api
+      .put(`/producttype/${product.id}`, product)
+      .then(() => {
+        toast.success("La categoría de prodcuto se editó con éxito");
+        dispatch(success(product));
+        dispatch(setLoading(false));
+        return dispatch(goBack());
+      })
+      .catch(error => {
+        apiErrorToast(error);
+        return dispatch(setLoading(false));
+      });
+  };
+}
