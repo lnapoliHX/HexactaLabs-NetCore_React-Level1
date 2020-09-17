5b7464487fc9e58dc0896c0eb52366d2c3858659
Christian Bancalá <c.bancala@outlook.com>
2020-09-17 16:56:49 -0300

    Corrección UI

--- Stock.Web/client-app/src/index.js
+++ Stock.Web/client-app/src/index.js
@@ -44,7 +44,7 @@ const menu_factory = (title, to, icon, children = []) => ({
 const menu = [
   menu_factory("Proveedor", "/provider", "algun-icon"),
   menu_factory("Tiendas", "/store", "algun-icon"),
-  menu_factory("Tipos de producto", "/product-type", "algun-icon")
+  menu_factory("Categorías", "/product-type", "algun-icon")
 ];
 
 ReactDOM.render(
