e595e8c147439fb7a804c686f073929c9177aed4
franco dreher <dreheragustin@hotmail.com>
2020-09-18 14:46:08 -0300

    funcionalidades categoria de productos

---
+++ Stock.Web/client-app/src/modules/categories/form/presentation/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Field, reduxForm } from "redux-form";
+import { Form, Button } from "reactstrap";
+import Validator from "../../../../common/helpers/YupValidator";
+import InputField from "../../../../components/inputs/InputField";
+import schema from "../validation";
+
+const CategoryForm = props => {
+  const { handleSubmit, handleCancel } = props;
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Field label="Iniciales" name="initials" component={InputField} type="text" />
+      <Field
+        label="Descripcion"
+        name="description"
+        component={InputField}
+        type="text"
+      />
+      <Button className="category-form__button" color="primary" type="submit">
+        Guardar
+      </Button>
+      <Button
+        className="category-form__button"
+        color="secondary"
+        type="button"
+        onClick={handleCancel}
+      >
+        Cancelar
+      </Button>
+    </Form>
+  );
+};
+
+CategoryForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleCancel: PropTypes.func.isRequired
+};
+
+export default reduxForm({
+  form: "category",
+  validate: Validator(schema),
+  enableReinitialize: true
+})(CategoryForm);
---
+++ Stock.Web/client-app/src/modules/categories/list/index.js
@@ -0,0 +1,141 @@
+//import { pickBy } from "lodash";
+import api from "../../../common/api";
+import { apiErrorToast } from "../../../common/api/apiErrorToast";
+
+const initialState = {
+  loading: false,
+  categories: []
+};
+
+/* Action types */
+
+const LOADING = "CATEGORIES_LOADING";
+const SET = "CATEGORIES_SET";
+const CREATE = "CATEGORIES_CREATE";
+const UPDATE = "CATEGORIES_UPDATE";
+const REMOVE = "CATEGORIES_REMOVE";
+
+export const ActionTypes = {
+  LOADING,
+  SET,
+  CREATE,
+  UPDATE,
+  REMOVE
+};
+
+/* Reducer handlers */
+function handleLoading(state, { loading }) {
+  return {
+    ...state,
+    loading
+  };
+}
+
+function handleSet(state, { categories }) {
+  return {
+    ...state,
+    categories
+  };
+}
+
+function handleNewCategory(state, { category }) {
+  return {
+    ...state,
+    categories: state.categories.concat(category)
+  };
+}
+
+function handleUpdateCategory(state, { category }) {
+  return {
+    ...state,
+    categories: state.categories.map(s => (s.id === category.id ? category : s))
+  };
+}
+
+function handleRemoveCategory(state, { id }) {
+  return {
+    ...state,
+    categories: state.categories.filter(s => s.id !== id)
+  };
+}
+
+const handlers = {
+  [LOADING]: handleLoading,
+  [SET]: handleSet,
+  [CREATE]: handleNewCategory,
+  [UPDATE]: handleUpdateCategory,
+  [REMOVE]: handleRemoveCategory
+};
+
+export default function reducer(state = initialState, action) {
+  const handler = handlers[action.type];
+  return handler ? handler(state, action) : state;
+}
+
+/* Actions */
+export function setLoading(status) {
+  return {
+    type: LOADING,
+    loading: status
+  };
+}
+
+export function setCategories(categories) {
+  return {
+    type: SET,
+    categories
+  };
+}
+
+export function getAll() {
+  return dispatch => {
+    dispatch(setLoading(true));
+    return api
+      .get("/producttype")
+      .then(response => {
+        dispatch(setCategories(response.data));
+        return dispatch(setLoading(false));
+      })
+      .catch(error => {
+        apiErrorToast(error);
+        return dispatch(setLoading(false));
+      });
+  };
+}
+
+export function getById(id) {
+  return getAll({ id });
+}
+
+/*
+export function fetchByFilters(filters) {
+  return function(dispatch) {
+    return api
+      .post("/store/search", pickBy(filters))
+      .then(response => {
+        dispatch(setCategories(response.data));
+      })
+      .catch(error => {
+        apiErrorToast(error);
+      });
+  };
+}
+*/
+
+/* Selectors */
+function base(state) {
+  console.log(state);
+  return state.category.list;
+}
+
+export function getLoading(state) {
+  return base(state).loading;
+}
+
+export function getCategories(state) {
+  return base(state).categories;
+}
+
+export function getCategoryById(state, id) {
+  return getCategories(state).find(s => s.id === id);
+}
