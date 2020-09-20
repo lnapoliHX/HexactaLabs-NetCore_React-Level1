af1284ecff2e01b3b93a2a8b9ab8ec58b7e53a33
NicoAcevedo <acevedo_nico@hotmail.com>
2020-09-20 18:12:51 -0300

    Level 1

--- Stock.AppService/Services/ProductTypeService.cs
+++ Stock.AppService/Services/ProductTypeService.cs
@@ -9,6 +9,28 @@ namespace Stock.AppService.Services
         public ProductTypeService(IRepository<ProductType> repository)
             : base(repository)
         {
+
         }
+
+        public new ProductType Create(ProductType entity)
+        {
+            if (this.NombreUnico(entity.Initials))
+            {
+                return base.Create(entity);
+            }
+
+            throw new System.Exception("The Initials is already in use");
+        }
+        private bool NombreUnico(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return false;
+            }
+
+            return this.Repository.List(x => x.Initials.ToUpper().Equals(name.ToUpper())).Count == 0;
+        }
+
+        
     }
 }
---
+++ Stock.Web/client-app/src/modules/productTypes/list/container/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+import PropTypes from "prop-types";
+import { getProductTypes, getAll, fetchByFilters } from "../index";
+import Presentation from "../presentation";
+
+class ProductTypesPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filters: {
+        initials: "",
+        description: "",
+        condition: "AND"
+      }
+    };
+  }
+
+  filterChanged = event => {
+    const newFilters = {
+      ...this.state.filters,
+      [event.target.initials]: event.target.value
+    };
+    this.setState({ filters: newFilters });
+  };
+
+  render() {
+    return (
+      <Presentation
+        data={this.props.productTypes}
+        dataLoading={this.props.loading}
+        defaultPageSize={5}
+        filters={this.state.filters}
+        handleFilter={this.filterChanged}
+        submitFilter={() => this.props.fetchByFilters(this.state.filters)}
+        clearFilter={this.props.getAll}
+        {...this.props}
+      />
+    );
+  }
+}
+
+ProductTypesPage.propTypes = {
+  productTypes: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => {
+  return { productTypes: getProductTypes(state) };
+};
+
+const mapDispatchToProps = {
+  getAll,
+  push,
+  fetchByFilters
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductTypesPage);
