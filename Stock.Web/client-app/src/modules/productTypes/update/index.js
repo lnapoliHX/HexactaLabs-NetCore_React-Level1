a1ff354480be3aeeb0a7a80cbaf964ecb5a04469
espisano <pisano5@hotmail.com>
2021-12-01 16:14:21 -0300

    Create product type module

---
+++ Stock.Web/client-app/src/modules/productTypes/update/index.js
@@ -0,0 +1,31 @@
+import api from "../../../common/api";
+import { apiErrorToast } from "../../../common/api/apiErrorToast";
+import { setLoading, ActionTypes } from "../list";
+import { toast } from "react-toastify";
+import { goBack } from "connected-react-router";
+
+/* Actions */
+function success(productType) {
+  return {
+    type: ActionTypes.UPDATE,
+    productType
+  };
+}
+
+export function update(productType) {
+  return function(dispatch) {
+    dispatch(setLoading(true));
+    return api
+      .put(`/productType/${productType.id}`, productType)
+      .then(() => {
+        toast.success("El proveedor se editó con éxito");
+        dispatch(success(productType));
+        dispatch(setLoading(false));
+        return dispatch(goBack());
+      })
+      .catch(error => {
+        apiErrorToast(error);
+        return dispatch(setLoading(false));
+      });
+  };
+}
